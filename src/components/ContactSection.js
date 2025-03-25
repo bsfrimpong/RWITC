34e3f2d591525ff8b284f28666dcff48767c66ad
bsfrimpong <62362646+bsfrimpong@users.noreply.github.com>
2025-03-25 09:06:10 +0000

    Update ContactSection.js

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Mail, Phone, MapPin, MessageCircle, ArrowRight } from 'lucide-react';
 
 const ContactSection = () => {
-  const email = "realworldtechinnovationcenter@gmail.com";
+  const email = "contact@realworldtechcenter.com";
   const phoneNumber = "+233246756644";
   const whatsappMessage = "Hello! I'm interested in learning more about your STEM education programs.";
 
